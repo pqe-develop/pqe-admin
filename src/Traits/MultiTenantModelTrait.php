da6eb871f0f11f9432942ea407d30b8b9965f64b
PQE <100579729+pqe-develop@users.noreply.github.com>
2023-07-04 18:35:45 +0200

    Update MultiTenantModelTrait.php

--- src/Traits/MultiTenantModelTrait.php
+++ src/Traits/MultiTenantModelTrait.php
@@ -11,12 +11,36 @@ trait MultiTenantModelTrait {
         if (!app()->runningInConsole() && auth()->check()) {
 
             if (!GetAdmin::getAdmin() && auth()->user()->teams->count() > 0) {
-                static::addGlobalScope('team_id', function (Builder $builder) {
-                            $field = sprintf('%s.%s', $builder->getQuery()->from, 'team_id');
+                static::addGlobalScope('team_id',
+                        function (Builder $builder) {
                     foreach (auth()->user()->teams->toArray() as $item) {
                         $arrayTeams[] = $item['id'];
                     }
+                            $field = sprintf('%s.%s', $builder->getQuery()->from, 'team_id');
+                            if ($field == 'resources.team_id') {
                     $builder->whereIn($field, $arrayTeams)->orWhere($field, Null);
+//                             } else {
+//                                 // $builder->join('resources', 'resources.id', $fieldJoin)
+//                                 // ->whereIn($fieldTeam, $arrayTeams)->orWhere($fieldTeam, Null)
+//                                 // ->whereNull('resources.deleted_at');
+
+//                                 // $builder->addSelect(
+//                                 // [
+//                                 // 'resource_code_id' => Resource::select('id')->whereColumn('id', $fieldJoin)->whereIn(
+//                                 // $fieldTeam, $arrayTeams)->orWhere($fieldTeam, Null)->whereNull(
+//                                 // 'resources.deleted_at')
+//                                 // ]);
+
+//                                 $builder->whereIn('resource_code_id',
+//                                         function ($query) {
+//                                             foreach (auth()->user()->teams->toArray() as $item) {
+//                                                 $arrayTeams[] = $item['id'];
+//                                             }
+//                                             $query->select('id')->from('resources')->whereIn('resources.team_id',
+//                                                     $arrayTeams)->orWhere('resources.team_id', Null)->whereNull(
+//                                                     'resources.deleted_at');
+//                                         });
+                            }
                         });
             }
         }

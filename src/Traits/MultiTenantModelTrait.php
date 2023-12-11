bd0d8346723a9bd56b5591c4d7ba505fd6e19c1d
Alessandro Badii <a.badii@pqegroup.com>
2023-12-11 12:33:17 +0100

    Trait SoftPqeDeletes added

--- src/Traits/MultiTenantModelTrait.php
+++ src/Traits/MultiTenantModelTrait.php
@@ -11,17 +11,19 @@ trait MultiTenantModelTrait {
         if (!app()->runningInConsole() && auth()->check()) {
 
             if (!GetAdmin::getAdmin() && auth()->user()->teams->count() > 0) {
-                static::addGlobalScope('team_id', function (Builder $builder) {
-                    foreach (auth()->user()->teams->toArray() as $item) {
-                        $arrayTeams[] = $item['id'];
+                static::addGlobalScope('team_id',
+                        function (Builder $builder) {
+                            foreach (auth()->user()->teams->toArray() as $item) {
+                                $arrayTeams[] = $item['id'];
                                 $arrayCond[] = "'" . $item['id'] . "'";
-                    }
+                            }
                             $arrayCond = implode(",", $arrayCond);
                             $field = sprintf('%s.%s', $builder->getQuery()->from, 'team_id');
                             if ($field == 'resources.team_id') {
-                    $builder->whereIn($field, $arrayTeams)->orWhere($field, Null);
+                                $builder->whereIn($field, $arrayTeams)->orWhere($field, Null);
                             } else {
-                        $builder->whereRaw('resource_code_id in (select id from resources where isnull(deleted_at) and (team_id in (' .
+                                $builder->whereRaw(
+                                        'resource_code_id in (select id from resources where isnull(deleted_at) and (team_id in (' .
                                         $arrayCond . ') or team_id is null))');
                             }
                         });
